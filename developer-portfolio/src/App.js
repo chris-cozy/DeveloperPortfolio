f3e83f72496e3a4d3b8512a34bd4bd1eca063b5a
chris-cozy <csande9@clemson.edu>
2023-10-01 23:34:48 -0400

    Implemented footer

--- developer-portfolio/src/App.js
+++ developer-portfolio/src/App.js
@@ -5,6 +5,7 @@ import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -12,9 +13,13 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
+
       <Skills />
+
       <Projects />
+
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ developer-portfolio/src/components/Footer.js
@@ -0,0 +1,33 @@
+import { Container, Col, Row } from "react-bootstrap";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-item-center">
+          <Col sm={6}>
+            <img src={logo} alt="Logo" className="footer-logo" />
+          </Col>
+          <Col sm={6} className="text-center text-sm-end">
+            <div className="footer-social-icon">
+              <a href="">
+                <img src={navIcon1} />
+              </a>
+              <a href="">
+                <img src={navIcon2} />
+              </a>
+              <a href="">
+                <img src={navIcon3} />
+              </a>
+            </div>
+            <p>CopyRight 2023. All Rights Reserved by Cozy</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
