9ad37f146e9bf5a95b84d970c68d862b45f16f75
chris-cozy <csande9@clemson.edu>
2023-10-04 14:15:51 -0400

    Include lodash package for throttling, to improve effect performance

--- client/src/components/MouseTracker.js
+++ client/src/components/MouseTracker.js
@@ -1,40 +1,29 @@
 import React, { useEffect, useState } from "react";
-import "./MouseTracker.css"; // Import the CSS file for styling
+import "./MouseTracker.css";
+import _ from "lodash";
 
 export const MouseTracker = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [targetPosition, setTargetPosition] = useState({ x: 0, y: 0 });
 
-  const updateMousePosition = (e) => {
-    setTargetPosition({ x: e.clientX, y: e.clientY });
-  };
+  const updateMousePosition = _.throttle((e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  }, 40);
 
   useEffect(() => {
-    const updatePosition = () => {
-      setPosition((prevPosition) => ({
-        x: lerp(prevPosition.x, targetPosition.x, 0.1),
-        y: lerp(prevPosition.y, targetPosition.y, 0.1),
-      }));
-    };
-
     document.addEventListener("mousemove", updateMousePosition);
-    const animationFrameId = requestAnimationFrame(updatePosition);
 
     return () => {
       document.removeEventListener("mousemove", updateMousePosition);
-      cancelAnimationFrame(animationFrameId);
     };
-  }, [targetPosition]);
-
-  // Linear interpolation function
-  const lerp = (start, end, t) => {
-    return start * (1 - t) + end * t;
-  };
+  }, []);
 
   return (
     <div
       className="mouse-tracker"
-      style={{ left: `${position.x}px`, top: `${position.y}px` }}
+      style={{
+        left: `${position.x}px`, // Adjust the offset as needed
+        top: `${position.y}px`, // Adjust the offset as needed
+      }}
     />
   );
 };
