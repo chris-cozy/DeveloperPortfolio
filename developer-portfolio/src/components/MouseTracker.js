95adeb9095287a9eaf8b5e834e5f2adf180910ee
chris-cozy <csande9@clemson.edu>
2023-10-03 20:48:33 -0400

    Implemented mouse tracking gradient

---
+++ developer-portfolio/src/components/MouseTracker.js
@@ -0,0 +1,24 @@
+import React, { useRef, useEffect, useState } from "react";
+import "./MouseTracker.css"; // Import the CSS file for styling
+
+export const MouseTracker = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const updateMousePosition = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", updateMousePosition);
+    return () => {
+      document.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
+  return (
+    <div
+      className="mouse-tracker"
+      style={{ left: position.x, top: position.y }}
+    />
+  );
+};
