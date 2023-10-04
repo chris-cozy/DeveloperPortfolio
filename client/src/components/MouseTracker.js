fa2bbbf9e57ef374959319352864944c38ba0efd
chris-cozy <csande9@clemson.edu>
2023-10-04 13:42:34 -0400

    Updated mouse tracking for smoother movement

--- client/src/components/MouseTracker.js
+++ client/src/components/MouseTracker.js
@@ -3,22 +3,38 @@ import "./MouseTracker.css"; // Import the CSS file for styling
 
 export const MouseTracker = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [targetPosition, setTargetPosition] = useState({ x: 0, y: 0 });
 
   const updateMousePosition = (e) => {
-    setPosition({ x: e.clientX, y: e.clientY });
+    setTargetPosition({ x: e.clientX, y: e.clientY });
   };
 
   useEffect(() => {
+    const updatePosition = () => {
+      setPosition((prevPosition) => ({
+        x: lerp(prevPosition.x, targetPosition.x, 0.1),
+        y: lerp(prevPosition.y, targetPosition.y, 0.1),
+      }));
+    };
+
     document.addEventListener("mousemove", updateMousePosition);
+    const animationFrameId = requestAnimationFrame(updatePosition);
+
     return () => {
       document.removeEventListener("mousemove", updateMousePosition);
+      cancelAnimationFrame(animationFrameId);
     };
-  }, []);
+  }, [targetPosition]);
+
+  // Linear interpolation function
+  const lerp = (start, end, t) => {
+    return start * (1 - t) + end * t;
+  };
 
   return (
     <div
       className="mouse-tracker"
-      style={{ left: position.x, top: position.y }}
+      style={{ left: `${position.x}px`, top: `${position.y}px` }}
     />
   );
 };
